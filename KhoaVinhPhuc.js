16fb7c95767bb7b45f6f5d49ad8166d03af3cd0e
unknown <hoanghuuphuc300193@gmail.com>
2020-09-07 20:33:33 +0700

    Phúc Lam bai 1

--- KhoaVinhPhuc.js
+++ KhoaVinhPhuc.js
@@ -1,11 +1,8 @@
 //Vinh
-asdnjaksdnjkad
-ansdkjansdkjansjdnjaksd
-dfagadfgdfgdfgsdfg
+
 
 //Khoa
-khoa
+
 //Phuc
-for(let i =0; i< 9; i++){
-    console.log(i)
-}
+// 1.Learn how to create a random number from 0 to 1
+alert(Math.random().toFixed(2))
