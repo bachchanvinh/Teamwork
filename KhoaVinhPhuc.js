2b1e7c65c22893b360f7fff307b0260c924c878e
khoa071996@gmail.com <khoa071996@gmail.com>
2020-09-07 20:48:15 +0700

    K

--- KhoaVinhPhuc.js
+++ KhoaVinhPhuc.js
@@ -2,6 +2,16 @@
 
 
 //Khoa
+//Câu 2:2.Learn how to randomly pick an item from an array
+// For example:
+// Array: [2, 5, 6, 9, 10]
+// First run: 5
+// Second run: 9
+let Array= [2, 5, 6, 9, 10]
+console.log(Array)
+let random = Math.floor(Math.random() * Array.length)
+console.log(random)
+
 
 //Phuc
 // 1.Learn how to create a random number from 0 to 1
